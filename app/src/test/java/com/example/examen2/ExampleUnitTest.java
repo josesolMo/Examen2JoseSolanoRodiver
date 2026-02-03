8b96a8b6a782f90c2df8501d9b2e86f6444bc700
Rodiver-Baltodano <s.balto025@gmail.com>
2026-02-03 16:29:46 -0600

    Pruebas unitarias Rodiver

--- app/src/test/java/com/example/examen2/ExampleUnitTest.java
+++ app/src/test/java/com/example/examen2/ExampleUnitTest.java
@@ -1,17 +1,43 @@
 package com.example.examen2;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Pruebas unitarias para la clase Calculadora.
  */
 public class ExampleUnitTest {
+
+    // --- Pruebas para convertirNumero (entrada a decimal) ---
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testConvertirInvalido() {
+        assertEquals(-1, Calculadora.convertirNumero("GHI", 3));
+    }
+
+    @Test
+    public void testConvertirOctalADecimal() {
+        assertEquals(8, Calculadora.convertirNumero("10", 1));
+    }
+
+    // --- Pruebas para realizarOperacion ---
+    @Test
+    public void testMultiplicacion() {
+        assertEquals(50, Calculadora.realizarOperacion(10, 5, "*"));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivisionPorCero() {
+        Calculadora.realizarOperacion(10, 0, "/");
+    }
+
+    // --- Pruebas para convertirADestino (decimal a salida) ---
+    @Test
+    public void testDecimalAOctal() {
+        assertEquals("12", Calculadora.convertirADestino(10, 1));
+    }
+
+    @Test
+    public void testDecimalADecimal() {
+        assertEquals("100", Calculadora.convertirADestino(100, 2));
     }
 }
